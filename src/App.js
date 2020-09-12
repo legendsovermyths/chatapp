5e7404e34997be692c4b74e513762f0d7d31bbd4
Mask Wolf <maskwolf2000@gmail.com>
2020-09-12 10:41:39 +0530

    sidebar start

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className='app'>
       <div className='app__body'>
-        <h1>Let's do this!</h1>
         <Sidebar></Sidebar>
         <Chat></Chat>
       </div>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,35 @@
 import React from "react";
+import "./Sidebar.css";
+import Avatar from "@material-ui/core/Avatar";
+import ChatIcon from "@material-ui/icons/Chat";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <h1>I am chat component</h1>
+      <div className='sidebar__header'>
+        <div className='sidebar__header-avatar'>
+          <Avatar
+            alt='Remy Sharp'
+            src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+          />
+        </div>
+        <div className='sidebar__header-options'>
+          <ChatIcon className='sidebar__header-option'></ChatIcon>
+          <DonutLargeIcon className='sidebar__header-option'></DonutLargeIcon>
+          <MoreVertIcon className='sidebar__header-option'></MoreVertIcon>
+        </div>
+      </div>
+      <div className='sidebar__search'>
+        <SearchIcon className='sidebar__search-icon'></SearchIcon>
+        <input
+          type='text'
+          placeholder='Seach'
+          className='sidebar__search-text'
+        />
+      </div>
     </div>
   );
 }
