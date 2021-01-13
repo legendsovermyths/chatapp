638221aabb3a2db35887d02ac502956758827bb1
Mask Wolf <maskwolf2000@gmail.com>
2021-01-13 22:05:01 +0530

    input bar done

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 import SearchSharpIcon from "@material-ui/icons/SearchSharp";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
@@ -27,9 +27,18 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Chat() {
+  const [input, setInput] = useState("");
+  const [avatar_url, setAvatar_url] = useState("");
   const classes = useStyles();
-  var avatar = Math.floor(Math.random() * 100);
-  let avatar_url = `https://avatars.dicebear.com/api/avataaars/${avatar}.svg`;
+  useEffect(() => {
+    var avatar = Math.floor(Math.random() * 100);
+    setAvatar_url(`https://avatars.dicebear.com/api/avataaars/${avatar}.svg`);
+  }, []);
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("u typed", input);
+  };
+
   return (
     <div className='chat'>
       <div className='chat__header'>
@@ -57,12 +66,21 @@ function Chat() {
           <ChatMessage></ChatMessage>
         </div>
         <div className='chat__input-bar'>
-          <EmojiEmotionsOutlinedIcon />
+          <EmojiEmotionsOutlinedIcon
+            style={{ color: "white", padding: "7px" }}
+          />
           <form action='submit'>
-            <input type='text' />
-            <button type='submit'></button>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type='text'
+              placeholder='Type a message'
+            />
+            <button onClick={(e) => sendMessage(e)} type='submit'>
+              Send
+            </button>
           </form>
-          <MicNoneOutlinedIcon />
+          <MicNoneOutlinedIcon style={{ color: "#eeeeee", padding: "7px" }} />
         </div>
       </div>
     </div>
